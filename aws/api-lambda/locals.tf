ee1f2f616517e529b1f085961044119e0166cf64
wwalpha <wwalpha@gmail.com>
2019-09-09 19:41:48 +0900

    bug fix

--- aws/api-lambda/locals.tf
+++ aws/api-lambda/locals.tf
@@ -3,8 +3,9 @@ locals {
   lambda_full_name           = "${var.project_name}-${var.lambda_function_name}"
   lambda_default_role_name   = "${var.project_name}_${var.lambda_function_name}Role"
   lambda_role_name           = "${var.lambda_role_name != null ? var.lambda_role_name : local.lambda_default_role_name}"
-  lambda_uri_prefix           = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${module.lambda.arn}"
+  lambda_uri_prefix          = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${module.lambda.arn}"
   lambda_uri                 = "${var.lambda_alias_name != null ? "${local.lambda_uri_prefix}:${var.lambda_alias_name}/invocations" : "${local.lambda_uri_prefix}/invocations"}"
+  lambda_enable_dummy        = "${var.lambda_filename != null ? false : var.lambda_enable_dummy}"
   count_api_gateway_resource = "${var.method_path_part != "" ? 1 : 0}"
 
   resource_path_src       = "${local.count_api_gateway_resource > 0 ? aws_api_gateway_resource.this.0.path : var.method_parent_path}"
