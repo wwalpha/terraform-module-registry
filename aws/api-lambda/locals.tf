b0ec26cbc17c62ffc6b9521501cc34730771b5a7
wwalpha <wwalpha@gmail.com>
2019-09-09 18:05:29 +0900

    bug fix

--- aws/api-lambda/locals.tf
+++ aws/api-lambda/locals.tf
@@ -3,10 +3,8 @@ locals {
   lambda_full_name           = "${var.project_name}-${var.lambda_function_name}"
   lambda_default_role_name   = "${var.project_name}_${var.lambda_function_name}Role"
   lambda_role_name           = "${var.lambda_role_name != null ? var.lambda_role_name : local.lambda_default_role_name}"
-  lambda_has_alias           = "${var.lambda_alias_name != null}"
-  lambda_uri_alias           = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${module.lambda.arn}:${var.lambda_alias_name}/invocations"
-  lambda_uri_no_alias        = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${module.lambda.arn}/invocations"
-  lambda_uri                 = "${local.lambda_has_alias ? local.lambda_uri_alias : local.lambda_uri_no_alias}"
+  lambda_uri_prefix           = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${module.lambda.arn}"
+  lambda_uri                 = "${var.lambda_alias_name != null ? "${local.lambda_uri_prefix}:${var.lambda_alias_name}/invocations" : "${local.lambda_uri_prefix}/invocations"}"
   count_api_gateway_resource = "${var.method_path_part != "" ? 1 : 0}"
 
   resource_path_src       = "${local.count_api_gateway_resource > 0 ? aws_api_gateway_resource.this.0.path : var.method_parent_path}"
