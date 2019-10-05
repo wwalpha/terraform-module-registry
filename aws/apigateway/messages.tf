277b3830720f5477afbf22fab85b314ae79e0a27
wwalpha <wwalpha@gmail.com>
2019-10-06 01:22:02 +0900

    remove repository

--- aws/apigateway/messages.tf
+++
@@ -1,7 +0,0 @@
-# -------------------------------------------------------
-# Provider arns is required
-# -------------------------------------------------------
-# resource "null_resource" "authorizer_provider_arns_required" {
-#   count = "${var.authorizer_type == "COGNITO_USER_POOLS" && local.authorizer_provider_arns == null ? 1 : 0}"
-#   "ERROR: variable authorizer_provider_arns is required. " = true
-# }
