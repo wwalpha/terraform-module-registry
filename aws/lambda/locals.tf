1d90cb89b894026affd614a1e440f4aed0c8466e
Alpha <wwalpha@gmail.com>
2022-09-12 10:08:20 +0900

    Update locals.tf

--- aws/lambda/locals.tf
+++ aws/lambda/locals.tf
@@ -5,15 +5,15 @@ locals {
   empty_array = []
   empty_map   = {}
 
-  dead_letter_config      = "${var.target_arn != null ? tolist[var.target_arn] : local.empty_array}"
-  environment             = "${var.variables != null ? tolist[var.variables] : local.empty_array}"
+  dead_letter_config      = "${var.target_arn != null ? tolist([var.target_arn]) : local.empty_array}"
+  environment             = "${var.variables != null ? tolist([var.variables]) : local.empty_array}"
   mode                    = "${var.xray_enabled ? "Active" : var.mode}"
-  tracing_config          = "${local.mode != null ? tolist[local.mode] : local.empty_array}"
+  tracing_config          = "${local.mode != null ? tolist([local.mode]) : local.empty_array}"
   vpc_config              = "${var.subnet_ids != null ? map("subnet_ids", var.subnet_ids, "security_group_ids", var.security_group_ids) : local.empty_map}"
-  routing_config          = "${var.additional_version_weights != null ? tolist[var.additional_version_weights] : local.empty_array}"
+  routing_config          = "${var.additional_version_weights != null ? tolist([var.additional_version_weights]) : local.empty_array}"
   filename                = "${var.dummy_enabled ? "${path.module}/index.zip" : var.filename}"
   source_code_hash        = "${var.dummy_enabled ? "${filebase64sha256("${path.module}/index.zip")}" : var.source_code_hash}"
-  ignore_changes          = "${var.publish ? tolist["source_code_hash", "qualified_arn", "version", "last_modified"] : null}"
+  ignore_changes          = "${var.publish ? tolist(["source_code_hash", "qualified_arn", "version", "last_modified"]) : null}"
   aws_lambda_function     = "${var.dummy_enabled ? aws_lambda_function.dummy[0] : aws_lambda_function.this[0]}"
   aws_xray_write_only_arn = "arn:aws:iam::aws:policy/AWSXrayWriteOnlyAccess"
 
